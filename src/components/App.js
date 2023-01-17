63d7593533d397f803e072302d07678e87bc7010
Andrew Burlachenko <ap.burla4enko@gmail.com>
2023-01-17 16:12:35 +0300

    replaced classes to hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import { nanoid } from 'nanoid';
-import { Component } from 'react';
+import { useState, useEffect } from 'react';
 import { toast, Toaster } from 'react-hot-toast';
 import { ContactForm } from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
@@ -7,39 +7,31 @@ import { Filter } from './Filter/Filter';
 import { GlobalStyle } from './GlobalStyle';
 
 const LS_KEY = 'contacts';
+const initContacts = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+  { id: 'id-5', name: 'Roy Jones', number: '285-11-86' },
+];
 
-export class App extends Component {
-  state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-      { id: 'id-5', name: 'Roy Jones', number: '285-11-86' },
-    ],
-    filter: '',
-  };
-
-  componentDidMount() {
-    const savedContact = JSON.parse(localStorage.getItem(LS_KEY));
-    savedContact && this.setState({ contacts: [...savedContact] });
-  }
-
-  componentDidUpdate(_, prevState) {
-    const { contacts } = this.state;
-    if (prevState.contacts.length !== contacts.length) {
-      localStorage.setItem(LS_KEY, JSON.stringify(contacts));
+export const App = () => {
+  const [contacts, setContacts] = useState(() => {
+    const savedContacts = localStorage.getItem(LS_KEY);
+    if (savedContacts !== null) {
+      const parcedContacts = JSON.parse(savedContacts);
+      return parcedContacts;
     }
-  }
+    return initContacts;
+  });
+  const [filter, setFilter] = useState('');
 
-  isInContact = (contacts, newName) => {
-    return contacts.filter(({ name }) =>
-      name.toLowerCase().includes(newName.toLowerCase().trim())
-    );
-  };
+  useEffect(() => {
+    localStorage.setItem(LS_KEY, JSON.stringify(contacts));
+  }, [contacts]);
 
-  addContact = ({ name, number }, { resetForm }) => {
-    if (this.isInContact(this.state.contacts, name).length > 0) {
+  const addContact = ({ name, number }, { resetForm }) => {
+    if (isInContact(contacts, name).length > 0) {
       return toast.error(`${name} is already in the contact list`);
     }
 
@@ -49,42 +41,42 @@ export class App extends Component {
       number,
     };
 
-    this.setState(prevState => ({
-      contacts: [...prevState.contacts, contact],
-    }));
+    setContacts(prevContacts => [...prevContacts, contact]);
     resetForm();
   };
 
-  filterContact = e => {
-    this.setState({ filter: e.currentTarget.value });
+  const deleteContact = contactId => {
+    setContacts(prevContacts =>
+      prevContacts.filter(contact => contact.id !== contactId)
+    );
   };
 
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
+  const visibleContacts = contacts.filter(contact =>
+    contact.name.toLowerCase().includes(filter.toLowerCase().trim())
+  );
+
+  const isInContact = (contacts, newName) => {
+    return contacts.filter(({ name }) =>
+      name.toLowerCase().includes(newName.toLowerCase().trim())
+    );
   };
 
-  render() {
-    const { filter, contacts } = this.state;
-    const visibleContacts = contacts.filter(contact =>
-      contact.name.toLowerCase().includes(filter.toLowerCase().trim())
-    );
-    return (
-      <>
-        <GlobalStyle />
-        <Toaster />
-        <div>
-          <h1>Phonebook</h1>
-          <ContactForm onSubmit={this.addContact} />
-          <h2>Contacts</h2>
-          <Filter value={filter} onChange={this.filterContact} />
-          <ContactList
-            contacts={!filter ? contacts : visibleContacts}
-            onClick={this.deleteContact}
-          />
-        </div>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <GlobalStyle />
+      <Toaster />
+      <div>
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={addContact} />
+        <h2>Contacts</h2>
+        {contacts.length > 0 && (
+          <Filter value={filter} onChange={e => setFilter(e.target.value)} />
+        )}
+        <ContactList
+          contacts={!filter ? contacts : visibleContacts}
+          onClick={deleteContact}
+        />
+      </div>
+    </>
+  );
+};
